7a0bcd833de6e88989c761507d96237798d0c19f
DESKTOP-LIZ-SS\lizbe <lizbeth.santamaria@epn.edu.ec>
2022-06-21 19:26:44 -0500

    Navegación entre actividades

---
+++ app/src/main/java/com/santamarializbeth/myapplication2/MainActivityPila1.kt
@@ -0,0 +1,28 @@
+package com.santamarializbeth.myapplication2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivityPila1 : AppCompatActivity() {
+    lateinit var buttonNavegar : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_pila1)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        buttonNavegar = findViewById(R.id.buttonNavegar)
+        buttonNavegar.setOnClickListener {
+            val intencion = Intent(this,MainActivityPila2::class.java)
+            startActivity(intencion)
+            finish()
+        }
+
+    }
+    override fun onSupportNavigateUp(): Boolean{
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+}
